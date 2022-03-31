27e4940504da1505ae78c6c61137fbf588de7e4a
vinoth <vinoth@gmail.com>
2022-03-31 17:12:59 +0530

    appC dag edited

--- dags/appC/appC-dag.py
+++ dags/appC/appC-dag.py
@@ -3,7 +3,7 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.version import version
-from datetime import datetime, timedelta # testline for appCfeature
+from datetime import datetime, timedelta # testline for appC-feature
 
 
 def my_custom_function(ts,**kwargs):
